f258e95b846a12bbbed2cd1193186b0703f3901b
merhasmukh <mhgn0001@gmail.com>
2025-04-14 23:43:10 +0530

    Add conversation context to ai bot

--- apis/settings.py
+++ apis/settings.py
@@ -222,6 +222,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+# Define the absolute filesystem path to the directory where collected static files will be stored.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
