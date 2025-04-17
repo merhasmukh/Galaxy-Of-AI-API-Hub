624136e71b433bf450b22e43c551fa1c43ad5c11
merhasmukh <mhgn0001@gmail.com>
2025-04-17 23:07:39 +0530

    chatbot enhancement

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,8 +5,8 @@ class Chat(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.CharField(max_length=255)  # You can also use ForeignKey if you're using Django's built-in User model
     chat_id = models.CharField(max_length=255)
-    name = models.CharField(max_length=20,default="") 
-    description = models.TextField(blank=True,default="")
+    name = models.CharField(max_length=20,default="AI Research Assistant") 
+    description = models.TextField(default="Project description")
     language = models.CharField(max_length=50,default="english")
     timestamp = models.DateTimeField(auto_now_add=True)
 
